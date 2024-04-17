fc67454d2f8271cd4d1d8c509265ca97ed0a1c3c
Joey Sabey <joey.sabey@gmx.com>
2024-04-17 05:47:59 +0100

    feat: add `Modf()` to `match.go`

--- maths/match.go
+++ maths/match.go
@@ -8,3 +8,9 @@ func Mod[T RationalNumber](x, y T) T {
    return T(math.Mod(float64(x), float64(y)))
 }
 
+// Modf returns integer and fractional floating-point numbers that sum to f. Both values have the same sign as f.
+func Modf[T RationalNumber](f T) (T, T) {
+   int, frac := math.Modf(float64(f))
+   return T(int), T(frac)
+}
+
