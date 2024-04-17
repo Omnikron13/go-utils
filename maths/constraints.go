502b03768485cb4f6bacfc0c3039220cd70aa8b7
Joey Sabey <joey.sabey@gmx.com>
2024-04-17 09:43:57 +0100

    feat: Rename `WholeNumber` constraint to `UnsignedInt`

--- maths/constraints.go
+++ maths/constraints.go
@@ -6,14 +6,14 @@
 package maths
 
 
-// WholeNumber is a type constraint that represents a whole number that is greater than or equal to zero.
-type WholeNumber interface { uint8 | uint16 | uint32 | uint64 | uint }
+// UnsignedInt is a type constraint that represents a whole number that is greater than or equal to zero.
+type UnsignedInt interface { uint8 | uint16 | uint32 | uint64 | uint }
 
 // Signed is a type constraint that represents a number that can be positive, negative, or zero.
 type Signed interface { int8 | int16 | int32 | int64 | int }
 
 // Integer is a type constraint that represents a whole number that can be positive, negative, or zero.
-type Integer interface { WholeNumber | Signed }
+type Integer interface { UnsignedInt | Signed }
 
 // Float is a type constraint that represents a floating-point number.
 type Float interface { float32 | float64 }
