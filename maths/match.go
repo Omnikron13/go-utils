320cfcdd877f6f2e22ad083dc3f9429fbce07848
Joey Sabey <joey.sabey@gmx.com>
2024-04-17 08:26:57 +0100

    feat: add `Min()` & `Max()` to `match.go`

--- maths/match.go
+++ maths/match.go
@@ -11,6 +11,16 @@ func Abs[T RationalNumber](x T) T {
    return -x
 }
 
+// Max returns the larger of x or y.
+func Max[T RationalNumber](x, y T) T {
+   return max(x, y)
+}
+
+// Min returns the smaller of x or y.
+func Min[T RationalNumber](x, y T) T {
+   return min(x, y)
+}
+
 // TODO: Expand this to include additional float types (float16, and perhaps float128/float8 of some kind..?)
 // Mod returns the remainder of x/y.
 func Mod[T RationalNumber](x, y T) T {
