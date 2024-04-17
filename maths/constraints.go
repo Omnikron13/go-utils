3b9bc0546922c4e89a9a311617f6554f226cac2f
Joey Sabey <joey.sabey@gmx.com>
2024-04-17 09:46:56 +0100

    feat: Rename `Signed` constraint to `SignedInt`

--- maths/constraints.go
+++ maths/constraints.go
@@ -9,11 +9,11 @@ package maths
 // UnsignedInt is a type constraint that represents a whole number that is greater than or equal to zero.
 type UnsignedInt interface { uint8 | uint16 | uint32 | uint64 | uint }
 
-// Signed is a type constraint that represents a number that can be positive, negative, or zero.
-type Signed interface { int8 | int16 | int32 | int64 | int }
+// SignedInt is a type constraint that represents a number that can be positive, negative, or zero.
+type SignedInt interface { int8 | int16 | int32 | int64 | int }
 
 // Integer is a type constraint that represents a whole number that can be positive, negative, or zero.
-type Integer interface { UnsignedInt | Signed }
+type Integer interface { UnsignedInt | SignedInt }
 
 // Float is a type constraint that represents a floating-point number.
 type Float interface { float32 | float64 }
