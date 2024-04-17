93c04e7f547188b790e07b69aab739b32cdaf3cc
Joey Sabey <joey.sabey@gmx.com>
2024-04-17 08:11:19 +0100

    chore: remove `abs_test.go`, add `Test_Abs()` to `match_test.go`

--- maths/match_test.go
+++ maths/match_test.go
@@ -24,6 +24,66 @@ var tests map[string]any = map[string]any {
    "float64": []float64{+0, -0, -1, 1, -9223372036854775807, -9223372036854775808, 9223372036854775806,  9223372036854775807},
 }
 
+// TODO: document/comment this
+// TODO: refactor this to not require the test string - should be able to infer it from the type constraint T
+func testMatrix[T RationalNumber](t *testing.T, test string, f func(t *testing.T, v T)) {
+   if v, ok := tests[test].([]T); ok {
+      t.Run(test, func(t *testing.T) {
+         for _, x := range v {
+            f(t, T(x))
+         }
+      })
+   }
+}
+
+// TODO: add benchmarkMatrix[T RationalNumber](b *testing.B, test string, f func(b *testing.B, v T))
+
+// Test_Abs tests `Abs()` outputs against `math.Abs()` outputs.
+func Test_Abs(t *testing.T) {
+   testMatrix(t, "uint8", func(t *testing.T, x uint8) {
+      assert.Equal(t, Abs(x), uint8(math.Abs(float64(x))))
+   })
+   testMatrix(t, "uint16", func(t *testing.T, x uint16) {
+      assert.Equal(t, Abs(x), uint16(math.Abs(float64(x))))
+   })
+   testMatrix(t, "uint32", func(t *testing.T, x uint32) {
+      assert.Equal(t, Abs(x), uint32(math.Abs(float64(x))))
+   })
+   testMatrix(t, "uint64", func(t *testing.T, x uint64) {
+      // NOTE: uint64 is always positive, so Abs() should return the same value.
+      //       However math.Abs() requires casting to and from float64, which isn't capable of representing all uint64 values.
+      //       So we have to ignore what math.Abs() would do here, as our Abs() is more accurate.
+      assert.Equal(t, Abs(x), x)
+   })
+   testMatrix(t, "int8", func(t *testing.T, x int8) {
+      assert.Equal(t, Abs(x), int8(math.Abs(float64(x))))
+   })
+   testMatrix(t, "int16", func(t *testing.T, x int16) {
+      assert.Equal(t, Abs(x), int16(math.Abs(float64(x))))
+   })
+   testMatrix(t, "int32", func(t *testing.T, x int32) {
+      assert.Equal(t, Abs(x), int32(math.Abs(float64(x))))
+   })
+   testMatrix(t, "int64", func(t *testing.T, x int64) {
+      // NOTE: int64 is not always positive, so Abs() should invert negatives.
+      //       Again math.Abs() requires casting to and from float64, which isn't capable of representing all uint64 values.
+      //       So we have to ignore what math.Abs() would do here, as our Abs() is more accurate.
+      // TODO: handle signed integer overflows after Abs() is updated to handle them.
+      if x < 0 {
+         assert.Equal(t, Abs(x), -x)
+      } else {
+         assert.Equal(t, Abs(x), x)
+      }
+   })
+   testMatrix(t, "float32", func(t *testing.T, x float32) {
+      assert.Equal(t, Abs(x), float32(math.Abs(float64(x))))
+   })
+   testMatrix(t, "float64", func(t *testing.T, x float64) {
+      assert.Equal(t, Abs(x), math.Abs(x))
+   })
+}
+
+// TODO: refactor to use testMatrix() - after the refactor of testMatrix() is done!
 // Test_Mod tests `Mod()` outputs against `math.Mod()` outputs.
 func Test_Mod(t *testing.T) {
    for k, v := range tests {
