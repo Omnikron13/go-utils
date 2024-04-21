44b648cd7a42ded122f08a24428c24a64c08e318
Joey Sabey <joey.sabey@gmx.com>
2024-04-21 07:32:02 +0100

    feat: addded a batch of new wrappers to match.go
     - `Ceil()`
     - `Floor()`
     - `Log()`
     - `Log10()`
     - `Log1p()`
     - `Log2()`
     - `Logb()`
     - `Remainder()`
     - `Trunc()`
     - `Round()`
     - `RoundToEven()`

--- maths/match.go
+++ maths/match.go
@@ -11,9 +11,64 @@ func Abs[T Rational](x T) T {
    return -x
 }
 
+// Ceil returns the least integer value greater than or equal to x.
+func Ceil[T Float](x T) T {
+   return T(math.Ceil(float64(x)))
+}
+
 // Copysign returns a value with the magnitude of x and the sign of sign.
 func Copysign[T Signed](x, sign T) T {
    return T(math.Copysign(float64(x), float64(sign)))
+}
+
+// Floor returns the greatest integer value less than or equal to x.
+func Floor[T Float](x T) T {
+   return T(math.Floor(float64(x)))
+}
+
+// Log returns the natural logarithm of x.
+func Log[T Rational](x T) T {
+   return T(math.Log(float64(x)))
+}
+
+// Log10 returns the decimal logarithm of x.
+func Log10[T Rational](x T) T {
+   return T(math.Log10(float64(x)))
+}
+
+// Log1p returns the natural logarithm of 1 plus its argument.
+func Log1p[T Rational](x T) T {
+   return T(math.Log1p(float64(x)))
+}
+
+// Log2 returns the binary logarithm of x.
+func Log2[T Rational](x T) T {
+   return T(math.Log2(float64(x)))
+}
+
+// Logb returns the binary exponent of x.
+func Logb[T Rational](x T) T {
+   return T(math.Logb(float64(x)))
+}
+
+// Remainder returns the IEEE 754 floating-point remainder of x/y.
+func Remainder[T Float](x, y T) T {
+   return T(math.Remainder(float64(x), float64(y)))
+}
+
+// Trunc returns the integer value of x.
+func Trunc[T Rational](x T) T {
+   return T(math.Trunc(float64(x)))
+}
+
+// Round returns the nearest integer, rounding half away from zero.
+func Round[T Float](x T) T {
+   return T(math.Round(float64(x)))
+}
+
+// RoundToEven returns the nearest integer, rounding ties to even.
+func RoundToEven[T Float](x T) T {
+   return T(math.RoundToEven(float64(x)))
 }
 
 // Max returns the larger of x or y.
